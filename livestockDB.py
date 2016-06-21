4b219ee50a5f9e99376872b15d9fb3492d0eed91
Tim Plank <tim@soequestrian.com>
2016-06-21 14:07:30 -0700

    Create livestockDB.py

---
+++ livestockDB.py
@@ -0,0 +1,18 @@
+# livestockDB.py
+# Built with love using Python, tkinter, sqlite, and Github.
+# Please see LICENSE and README for more information.
+# kthxbai
+
+from tkinter import *
+from tkinter import ttk
+
+
+
+# main window
+root = Tk()
+root.title("livestockDB")
+
+
+
+# run main loop
+root.mainloop()
